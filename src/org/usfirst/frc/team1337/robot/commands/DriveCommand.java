47db41e733c1f565503d71ad54004080ce47036d
JGonzales <jed-gonzales@hotmail.com>
2018-01-07 15:36:15 -0500

    Large Drive Changes
    RoboDrive deprecated
    Used new DifferentialDrive class
    Using cheesy drive now

--- src/org/usfirst/frc/team1337/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team1337/robot/commands/DriveCommand.java
@@ -12,16 +12,16 @@ public class DriveCommand extends Command {
     public DriveCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.driveSubsystem.autoMove(0, 0);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveSubsystem.teleMove(Robot.oi.getJoystick1(), 1);
+    	Robot.driveSubsystem.joystickDrive(Robot.oi.getJoystick1().getRawAxis(1), Robot.oi.getJoystick1().getRawAxis(2));
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -31,7 +31,6 @@ public class DriveCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveSubsystem.autoMove(0, 0);
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc/team1337/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team1337/robot/subsystems/DriveSubsystem.java
@@ -1,55 +1,176 @@
 package org.usfirst.frc.team1337.robot.subsystems;
 
 import java.util.Vector;
-import org.usfirst.frc.team1337.robot.RobotMap;
 
+import org.usfirst.frc.team1337.robot.Robot;
+import org.usfirst.frc.team1337.robot.RobotMap;
+import org.usfirst.frc.team1337.robot.commands.DriveCommand;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class DriveSubsystem extends Subsystem {
-
-    RobotDrive robotDrive;
-    
-    TalonSRX backLeftDrive = new TalonSRX(RobotMap.backLeftDrive);
-    TalonSRX frontLeftDrive = new TalonSRX(RobotMap.frontLeftDrive);
-    TalonSRX backRightDrive = new TalonSRX(RobotMap.backRightDrive);
-    TalonSRX frontRightDrive = new TalonSRX(RobotMap.frontRightDrive);
+	
+	// AnalogInput leftEnc, rightEnc;
+	TalonSRX rightMaster, rightSlave, leftMaster, leftSlave;
+	Solenoid gearShiftSolenoid;
+	SpeedControllerGroup leftDrive, rightDrive;
+	DifferentialDrive myDrive;
+	PIDController encoderPID;
+	double speed;
+	private double kPE = 0.0001, kIE = 0.0000006, kDE = 0.00001; // Encoder PID
+	
 	
 //    public ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 
     DoubleSolenoid gearShift = new DoubleSolenoid(RobotMap.PCM, RobotMap.shotPinForward, RobotMap.shotPinReverse);
     
 	public DriveSubsystem() {
-		robotDrive = new RobotDrive(RobotMap.frontLeftDrive,RobotMap.backLeftDrive,RobotMap.frontRightDrive,RobotMap.backRightDrive);
+		try {
+		//initialize gyro
+		} catch (RuntimeException ex) {
+			DriverStation.reportError("Error instantiating gyro " + ex.getMessage(), true);
+		}
+
+		rightMaster = new TalonSRX(RobotMap.frontRightDrive);
+		leftMaster = new TalonSRX(RobotMap.frontLeftDrive);
+		rightSlave = new TalonSRX(RobotMap.backRightDrive);
+		leftSlave = new TalonSRX(RobotMap.backLeftDrive);
+	/*	leftSlave.set(ControlMode.Follower, leftMaster.getDeviceID());
+		rightSlave.set(ControlMode.Follower, rightMaster.getDeviceID());*/
 		
-		robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);
-		robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
-		robotDrive.setInvertedMotor(MotorType.kRearLeft, true);
-		robotDrive.setInvertedMotor(MotorType.kRearRight, true);
+		leftDrive=new SpeedControllerGroup((SpeedController)leftMaster, (SpeedController)leftSlave);
+		rightDrive = new SpeedControllerGroup((SpeedController)rightMaster, (SpeedController)rightSlave);
 		
-		robotDrive.setSafetyEnabled(true);
+		myDrive = new DifferentialDrive(leftDrive, rightDrive);
+
+		//rightMaster.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
+		//leftMaster.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
+		//rightMaster.setPosition(0);
+		//leftMaster.setPosition(0);
+		gearShiftSolenoid = new Solenoid(0);
+
+		//rightMaster.setProfile(0);
+		//leftMaster.setProfile(0);
+
+		// rightMaster.setPID(2.3, 0, 23);
+		// leftMaster.setPID(2.1, 0, 21);
+
+		//leftMaster.configPeakOutputVoltage(+12f, -12f);
+		//rightMaster.configPeakOutputVoltage(+12f, -12f);
+
+		//leftMaster.setNeutralMode(false);
+		//rightMaster.setNeutralMode(false);
 	}
 	
-	public void teleMove(Joystick joystick, double speed) {
-		robotDrive.arcadeDrive(joystick.getY()*speed, joystick.getX()*speed);
+	public void joystickDrive(double x, double y) {
+		myDrive.curvatureDrive(y, x, Robot.oi.getJoystick1().getRawButton(1));
+		
+		if (Robot.oi.getJoystick1().getRawButton(1)) {
+			shiftGears(true);
+		} else {
+			shiftGears(false);
+		}
 	}
 	
-	public void autoMove(double driveSpeed, double rotSpeed) {
-		robotDrive.arcadeDrive(driveSpeed, rotSpeed);
+	public void resetEnc() {
+	
+
 	}
+	
+	public void zeroGyro() {
+	}
+	
+	public void shiftGears(boolean shift) {
+		gearShiftSolenoid.set(shift);
+	}
+	
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new DriveCommand());
     }
+    
+    public void arcadeDrive(double speed, double rotate) {
+		myDrive.arcadeDrive(speed, rotate);
+	}
+
+
+	public void Setpoint(double ticks) {
+		encoderPID.setSetpoint(ticks);
+	}
+
+	public double sendSpeed() {
+		speed = Math.max(Math.min(0.8, speed), -0.8); // the double value can be
+
+		return speed;
+	}
+
+	public void resetEncPID() {
+		encoderPID.reset();
+	}
+
+	public boolean encOnTarget() {
+		return encoderPID.onTarget();
+	}
+
+	public void disableEncPID() {
+		encoderPID.disable();
+	}
+
+	/*public void initEncPID() {
+	
+		encoderPID = new PIDController(kPE, kIE, kDE, new PIDSource() {
+			PIDSourceType m_sourceType = PIDSourceType.kDisplacement;
+
+			@Override
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				m_sourceType = pidSource;
+			}
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return m_sourceType;
+			}
+
+			@Override
+			public double pidGet() {
+				return Robot.dr.getAvgEncoderDistance();
+			}
+		}, new PIDOutput() {
+			@Override
+			public void pidWrite(double pidSpeed) {
+				speed = pidSpeed;
+			}
+		});
+
+		encoderPID.setAbsoluteTolerance(25); // might need to be tuned if
+												// command never ends
+
+	}
+*/
+	public void enableEncPID() {
+		encoderPID.enable();
+	}
+    
 }
 
