20668f2c284b1ab1e2e9c0d9a69bb3bfdd856178
JGonzales <jed-gonzales@hotmail.com>
2018-01-07 23:05:11 -0500

    AutonDriveStraight Done?
    Probably

---
+++ src/org/usfirst/frc/team1337/robot/commands/AutonDriveStraight.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1337.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutonDriveStraight extends CommandGroup {
+
+    public AutonDriveStraight() {
+   addSequential(new DriveStraightToDistance(5000,10));
+   addSequential(new ZeroEncoders());
+    }
+}
