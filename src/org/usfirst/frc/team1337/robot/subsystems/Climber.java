f89c595f559de22a9ac2fac40ffea2aa55c35a2d
JGonzales <jed-gonzales@hotmail.com>
2018-01-08 20:40:15 -0500

    Changed Climber Victor to SRX

--- src/org/usfirst/frc/team1337/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team1337/robot/subsystems/Climber.java
@@ -4,6 +4,8 @@ import org.usfirst.frc.team1337.robot.Robot;
 import org.usfirst.frc.team1337.robot.RobotMap;
 import org.usfirst.frc.team1337.robot.commands.ClimbControl;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -15,8 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Climber extends Subsystem {
-	WPI_TalonSRX leftMaster, rightMaster, leftSlave;
-	Victor rightSlave;
+	WPI_TalonSRX leftMaster, rightMaster, leftSlave, rightSlave;
 	SpeedControllerGroup climbLeft, climbRight;
 
 	// Put methods for controlling this subsystem
@@ -25,11 +26,29 @@ public class Climber extends Subsystem {
 		leftMaster = new WPI_TalonSRX(RobotMap.LEFT_CLIMB_MASTER);
 		leftSlave = new WPI_TalonSRX(RobotMap.LEFT_CLIMB_SLAVE);
 		rightMaster = new WPI_TalonSRX(RobotMap.RIGHT_CLIMB_MASTER);
-		rightSlave = new Victor(RobotMap.RIGHT_CLIMB_SLAVE);
+		rightSlave = new WPI_TalonSRX(RobotMap.RIGHT_CLIMB_SLAVE);
 
-		climbLeft = new SpeedControllerGroup(leftMaster, leftSlave);
-		climbRight = new SpeedControllerGroup(rightMaster, rightSlave);
-		climbRight.setInverted(true);
+		//climbLeft = new SpeedControllerGroup(leftMaster, leftSlave);
+		//climbRight = new SpeedControllerGroup(rightMaster, rightSlave);
+		
+		leftSlave.set(ControlMode.Follower, leftMaster.getDeviceID());
+		rightMaster.set(ControlMode.Follower, leftMaster.getDeviceID());
+		rightSlave.set(ControlMode.Follower, leftMaster.getDeviceID());
+
+		
+		//climbRight.setInverted(true);
+		rightMaster.setInverted(true);
+		rightSlave.setInverted(true);
+		
+		leftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 5);
+		leftMaster.config_kP(0, 0.0, 0);
+		leftMaster.config_kI(0, 0.0, 0);
+		leftMaster.config_kD(0, 0.0, 0);
+		
+		leftMaster.configForwardSoftLimitThreshold(0, 0);
+		leftMaster.configReverseSoftLimitThreshold(0, 0);
+		leftMaster.configForwardSoftLimitEnable(true, 0);
+		leftMaster.configReverseSoftLimitEnable(true, 0);
 	}
 
 	public void initDefaultCommand() {
@@ -37,11 +56,26 @@ public class Climber extends Subsystem {
 	}
 
 	public void set(double speed) {
-		climbLeft.set(speed);
-		climbRight.set(speed);
+		//climbLeft.set(speed);
+		//climbRight.set(speed);
+		   if(Robot.oi.joystick2.getRawButton(1)){
+				leftMaster.set(ControlMode.Position, 0);
+		} else if (Robot.oi.joystick2.getRawButton(2)){
+			leftMaster.set(ControlMode.Position, 0);
+		} else if (Robot.oi.joystick2.getRawButton(3)) {
+			leftMaster.set(ControlMode.Position, 0);
+		} else if (Robot.oi.joystick2.getRawButton(4)) {
+			leftMaster.set(ControlMode.Position, 0);
+		} else if (Robot.oi.joystick2.getRawButton(5)) {
+			leftMaster.set(ControlMode.Position, 0);
+		} else {
+			leftMaster.set(ControlMode.PercentOutput, speed*0.25);
+		}
 	}
 	
 	public void logging() {
-		SmartDashboard.putNumber("Climb speed", climbLeft.get());
+		SmartDashboard.putNumber("climber pos", leftMaster.getSelectedSensorPosition(0));
+    	SmartDashboard.putNumber("climber preset", leftMaster.getActiveTrajectoryPosition());
+
 	}
 }
